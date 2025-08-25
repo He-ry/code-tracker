2845d65c53a7ab6d2d293a79189b97387d5097c8
He Ruyi <508816739@qq.com>
2025-08-25 16:51:48 +0800

    init

---
+++ tracker-ai/src/test/java/com/ai/TrackerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
