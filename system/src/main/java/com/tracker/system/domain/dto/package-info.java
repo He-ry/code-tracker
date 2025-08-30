85f0d91a99b6f8980a715399092375529ab5cc5b
He Ruyi <508816739@qq.com>
2025-08-30 23:17:43 +0800

    feat(system): 添加用户登录功能-

---
+++ system/src/main/java/com/tracker/system/domain/dto/package-info.java
@@ -0,0 +1,1 @@
+package com.tracker.system.domain.dto;
---
+++ system/src/main/java/com/tracker/system/service/LoginService.java
@@ -0,0 +1,15 @@
+package com.tracker.system.service;
+
+import com.tracker.system.domain.dto.login.LoginDTO;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+
+
+    public void login(LoginDTO loginDTO) {
+
+    }
+}
